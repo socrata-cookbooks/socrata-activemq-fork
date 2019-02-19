97426204d3dc73496187dc9ec8d865d7a73c2f78
Marc Slemko <marc.slemko@socrata.com>
2019-02-19 10:05:47 -0800

    Add maintainer_email which is apparently required even though the docs don't say that.

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,6 @@
 name 'socrata-activemq-fork'
 maintainer 'Socrata'
+maintainer_email 'sysadmin@socrata.com'
 license 'Apache-2.0'
 description 'Installs activemq and sets it up as service'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
